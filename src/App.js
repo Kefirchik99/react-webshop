06cce9c31c2dfb860db9dfb4f530dd99b144d961
Kefirchik99 <jermakent@gmail.com>
2025-10-21 23:34:21 +0300

    added json-db, implemented first "backend" logic

--- src/App.js
+++ src/App.js
@@ -1,129 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainPage } from './MainPage';
 import { FavoritePage } from './FavoritePage';
 
-const products = [
-  {
-    id: 1,
-    brand: 'Samsung',
-    name: 'S20',
-    price: 300,
-    category: 'phone',
-    rating: 5,
-    img: 'https://m.media-amazon.com/images/I/71Ji+UMQ-gL._AC_SL1498_.jpg'
-  },
-  {
-    id: 2,
-    brand: 'GPO',
-    name: '746',
-    price: 123,
-    category: 'phone',
-    rating: 4,
-    img: 'https://m.media-amazon.com/images/I/611PFZIL8CL._AC_SL1500_.jpg'
-  },
-  {
-    id: 3,
-    brand: 'Google',
-    name: 'Pixel 10',
-    price: 880,
-    category: 'phone',
-    rating: 4,
-    img: 'https://m.media-amazon.com/images/I/614Qf9iukZL._AC_SL1500_.jpg'
-  },
-  {
-    id: 4,
-    brand: 'Xiaomi',
-    name: 'A27I',
-    price: 100,
-    category: 'monitor',
-    rating: 5,
-    img: 'https://m.media-amazon.com/images/I/51LRt51aLEL._AC_SL1000_.jpg'
-  },
-  {
-    id: 5,
-    brand: 'TIVIQUE',
-    name: 'Gaming Laptop',
-    price: 999,
-    category: 'laptop',
-    rating: 2,
-    img: 'https://m.media-amazon.com/images/I/71+mst6l73L._AC_SL1500_.jpg'
-  },
-  {
-    id: 6,
-    brand: 'FUNYET',
-    name: 'Laptop',
-    price: 399,
-    category: 'laptop',
-    rating: 3,
-    img: 'https://m.media-amazon.com/images/I/71mYCPVN4kL._AC_SL1500_.jpg'
-  },
-  {
-    id: 7,
-    brand: 'Celeron',
-    name: 'N4000',
-    price: 350,
-    category: 'laptop',
-    rating: 4,
-    img: 'https://m.media-amazon.com/images/I/51fmoj3aFTL._AC_.jpg'
-  },
-  {
-    id: 8,
-    brand: 'Acer',
-    name: 'Aspire Go',
-    price: 390,
-    category: 'laptop',
-    rating: 2,
-    img: 'https://m.media-amazon.com/images/I/71dNQyfPveL._AC_SL1500_.jpg'
-  },
-  {
-    id: 9,
-    brand: 'LG',
-    name: '27MR',
-    price: 450,
-    category: 'monitor',
-    rating: 1,
-    img: 'https://m.media-amazon.com/images/I/71BydFEd1NL._AC_SL1500_.jpg'
-  },
-  {
-    id: 10,
-    brand: 'MSI',
-    name: 'PRO',
-    price: 85,
-    category: 'monitor',
-    rating: 4,
-    img: 'https://m.media-amazon.com/images/I/71moyya-AqL._AC_SL1500_.jpg'
-  },
-  {
-    id: 11,
-    brand: 'Hammer',
-    name: '6 LTE',
-    price: 50,
-    category: 'phone',
-    rating: 3,
-    img: 'https://m.media-amazon.com/images/I/71CO-ke0xdL._AC_SL1500_.jpg'
-  },
-  {
-    id: 12,
-    brand: 'Apple',
-    name: 'Iphone 15',
-    price: 450,
-    category: 'phone',
-    rating: 2,
-    img: 'https://m.media-amazon.com/images/I/61X5FknDWuL._AC_SL1500_.jpg'
-  }
-];
 
 function App() {
+
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
   const [inputName, setInputName] = useState('')
-  const [openNavbar, setOpenNavbar] = useState(false)
   const [selectedCategory, setSelectedCategory] = useState('')
   const [favId, setFavId] = useState([])
 
-  const filteredProduct = products.filter((el) =>
-    el.category.includes(selectedCategory) &&
-    el.name.toLowerCase().includes(inputName.toLowerCase()))
+  const [openNavbar, setOpenNavbar] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`http://localhost:5000/products?q=${inputName}&category_like=${selectedCategory}`)
+      .then((response) => response.json())
+      .then((result) => {
+        setLoading(false)
+        setProducts(result)
+      })
+      .catch((error) => console.log(error))
+  }, [inputName, selectedCategory])
 
   const handleInput = (text) => {
     setInputName(text)
@@ -162,16 +63,16 @@ function App() {
             openNavbar={openNavbar}
             handleChangeCategory={handleChangeCategory}
             selectedCategory={selectedCategory}
-            filteredProduct={filteredProduct}
+            products={products}
             addToFav={addToFav}
             favId={favId}
+            loading={loading}
           />
         }
         />
         <Route path='/favorites' element={<FavoritePage favProducts={favProducts} />} />
       </Routes>
     </div>
-
   )
 }
 
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -2,19 +2,30 @@ import { Header } from "./components/Header"
 import { Navbar } from "./components/Navbar"
 import { Card } from "./components/Card"
 
-export const MainPage = ({ openNavbar, handleInput, handleOpenNav, handleChangeCategory, selectedCategory, filteredProduct, addToFav, favId }) => {
+export const MainPage = ({
+    openNavbar,
+    handleInput,
+    handleOpenNav,
+    handleChangeCategory,
+    selectedCategory,
+    products,
+    addToFav,
+    favId,
+    loading
+}) => {
     return (
         <>
-            | <Header handleInput={handleInput} handleOpenNav={handleOpenNav} />
-            {
-                openNavbar &&
+            | <Header handleInput={handleInput}
+                handleOpenNav={handleOpenNav} />
+            {loading && <h3>loading...</h3>}
+            {openNavbar && (
                 <Navbar
                     handleChangeCategory={handleChangeCategory}
                     selectedCategory={selectedCategory}
                 />
-            }
+            )}
             <div className='card-container'>
-                {filteredProduct.map((el) => (
+                {products.map((el) => (
                     <Card
                         addToFav={addToFav}
                         favId={favId}
